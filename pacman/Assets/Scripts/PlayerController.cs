89ee2e0f81b67d876df3c5163839dc27b6ab8696
Andres Chavez <achav069@fiu.edu>
2020-09-25 00:00:55 -0400

    feat(pacman): Added player controls

---
+++ pacman/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+
+    private new Rigidbody2D rigidbody;
+
+    private new Transform transform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rigidbody = GetComponent<Rigidbody2D>();
+        this.transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        Vector2 movement = rigidbody.position;
+
+        if (Input.GetKey("up"))
+        {
+            movement.y++;
+        }
+
+        if (Input.GetKey("down"))
+        {
+            movement.y--;
+        }
+
+        if (Input.GetKey("right"))
+        {
+            movement.x++;
+        }
+
+        if (Input.GetKey("left"))
+        {
+            movement.x--;
+        }
+
+        //MovePosition literally moves the body to the position im passing which is bound to the movement 
+
+        rigidbody.MovePosition(movement * speed);
+    }
+}
